bdbda86452cebbc67ee93133906c92dba64ef52d
h1ggsb0s0n <53830849+h1ggsb0s0n@users.noreply.github.com>
2021-03-18 21:13:56 +0100

    Claims erstellt: IdentityprofileService hinzugefügt

--- MovieVoc/Server/Startup.cs
+++ MovieVoc/Server/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieVoc.Server.Helpers;
 using System.Linq;
 
 namespace MovieVoc.Server
@@ -37,7 +38,8 @@ namespace MovieVoc.Server
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
-                .AddApiAuthorization<IdentityUser, ApplicationDbContext>();
+                .AddApiAuthorization<IdentityUser, ApplicationDbContext>()
+                .AddProfileService<IdentityProfileService>();
 
             services.AddAuthentication()
                 .AddIdentityServerJwt();
